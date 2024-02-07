ba27d07b7b3457252d3fe916753667d1416422b4
Nikita Kharashun <tsurai7walkin@gmail.com>
2024-02-07 22:07:40 +0300

    Add project files.

---
+++ Animate-backend/Controllers/AuthController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Animate_backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+
+    }
+}
